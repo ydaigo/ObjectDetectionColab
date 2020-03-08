9238206e051a00cb4d6f7c202583ad94f50898cf
ydaigo <b1614808@planet.kanazawa-it.ac.jp>
2020-03-08 20:52:56 +0900

    a

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/open-mmlab/mmcv.git
 cd /content/mmcv
 pip install .  # (add "-e" if you want to develop or modify the codes)
-cd /content/ObjectDetectionColab/mmdetection/setup.py
-!pip install terminaltables==3.0.0
+cd /content/ObjectDetectionColab/mmdetection/
+pip install terminaltables==3.0.0
 python setup.py develop
+cd /content/ObjectDetectionColab/mmdetection
